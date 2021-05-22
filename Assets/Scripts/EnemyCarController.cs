b79ab0ea42ee7f00eaa5a4704e13de1129e53c86
Emanuel Kotzayan <emanuelkotzayan@gmail.com>
2021-05-22 19:05:35 +0200

    Using physics to move enemies (kind of)

--- Assets/Scripts/EnemyCarController.cs
+++ Assets/Scripts/EnemyCarController.cs
@@ -12,29 +12,40 @@ public class EnemyCarController : MonoBehaviour
     private GameController gameController;
     private Transform mainTarget;
 
-	void Awake () 
+	void Awake() 
     {
         this.rb = this.GetComponent<Rigidbody2D>();
         this.gameController = Object.FindObjectOfType<GameController>();
 	}
     
-	void Update () 
+    void Start()
+    {
+        this.forceUpdateWaypointIndex();
+    }
+    
+	void FixedUpdate() 
     {
         if (!this.gameController.isFighting)
         {
-            if (this.mainTarget != null) {
-                this.transform.position = Vector2.MoveTowards(this.transform.position,
-                    this.mainTarget.position,
-                    2f * Time.deltaTime);
+            if (this.mainTarget != null) 
+            {
+                this.rb.AddForce(this.mainTarget.position - transform.position);
             } 
             else if (this.waypointIndex <= this.waypoints.Length - 1)
             {
-                this.transform.position = Vector2.MoveTowards(this.transform.position,
-                this.waypoints[this.waypointIndex].position,
-                2f * Time.deltaTime);
+                var waypoint = this.waypoints[this.waypointIndex];
+                this.rb.AddForce(waypoint.position - transform.position);
+                
+                var rotation = Quaternion.LookRotation(waypoint.position);
+                rotation.x = 0f;
+                rotation.y = 0f;
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, 10f * Time.deltaTime);
 
-                if (this.transform.position == this.waypoints[this.waypointIndex].position)
+                Debug.Log($"this.transform.position {this.transform.position} waypoint.position {waypoint.position}");
+
+                if (this.isNearWaypoint(waypoint))
                 {
+                    Debug.Log("TRUE");
                     this.waypointIndex += 1;
                 }
             }
@@ -56,20 +67,31 @@ public class EnemyCarController : MonoBehaviour
 
         if (this.mainTarget == null)
         {
-            var closestWaypoint = 0;
-            float distanceToClosestWaypoint = Mathf.Abs(Vector2.Distance(this.transform.position, this.waypoints[closestWaypoint].position));
-            for (int i = 0; i < this.waypoints.Length; i++)
-            {
-                var waypoint = this.waypoints[i];
-                float distanceToWaypoint = Mathf.Abs(Vector2.Distance(this.transform.position, waypoint.position));
-                if (distanceToWaypoint <= distanceToClosestWaypoint)
-                {
-                    closestWaypoint = i;
-                    distanceToClosestWaypoint = distanceToWaypoint;
-                }
-            }
-
-            this.waypointIndex = closestWaypoint + 1;
+            this.forceUpdateWaypointIndex();
         }
+    }
+
+    private void forceUpdateWaypointIndex()
+    {
+        var closestWaypoint = 0;
+        float distanceToClosestWaypoint = Mathf.Abs(Vector2.Distance(this.transform.position, this.waypoints[closestWaypoint].position));
+        for (int i = 0; i < this.waypoints.Length; i++)
+        {
+            var waypoint = this.waypoints[i];
+            float distanceToWaypoint = Mathf.Abs(Vector2.Distance(this.transform.position, waypoint.position));
+            if (distanceToWaypoint <= distanceToClosestWaypoint)
+            {
+                closestWaypoint = i;
+                distanceToClosestWaypoint = distanceToWaypoint;
+            }
+        }
+
+        this.waypointIndex = closestWaypoint + 1;
+    }
+
+    private bool isNearWaypoint(Transform waypoint)
+    {
+        var rect = new Rect(waypoint.position.x, waypoint.position.y, 75, 75);
+        return rect.Contains(this.transform.position);
     }
 }
